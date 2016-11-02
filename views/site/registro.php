18c19cf4eb831493a05a5d6b577261436812ec8c
cloud-elric <humberto@2gom.com.mx>
2016-11-02 10:29:36 -0600

    Cambio de formulario

--- views/site/registro.php
+++ views/site/registro.php
@@ -1,20 +1,37 @@
 <?php
-use yii\widgets\ActiveForm;
+use yii\bootstrap\ActiveForm;
 use yii\helpers\Html;
 ?>
- <?php $form = ActiveForm::begin(['id' => 'registro-form', 'options' => ['class' => 'form-login']]); ?>
+ <?php $form = ActiveForm::begin(['id' => 'registro-form','options' => [ 
+				'enctype' => 'multipart/form-data' 
+		],
+ 		
+		'layout' => 'horizontal',
+		'fieldConfig' => [ 
+				'template' => "{input}\n{label}\n{error}",
+				'horizontalCssClasses' => [ 
+						'error' => 'mdl-textfield__error' 
+				],
+				'labelOptions' => [ 
+						'class' => 'mdl-textfield__label ' 
+				],
+				'options' => [ 
+						'class' => 'input-field col s12 m6' 
+				] 
+		],
+		'errorCssClass' => 'invalid' ]); ?>
 
-			<?= $form->field($usuario, 'txt_nombre',['options'=>['class'=>'animated']])->textInput(['autofocus' => true]) -> label(false) -> textInput(["placeholder" => "Nombre"])?>
+			<?= $form->field($usuario, 'txt_nombre', ['options'=>['class'=>'input-field col s12'], ['template'=>'{input}{label}']])->textInput(['autofocus' => true])-> textInput(["placeholder" => "Nombre"])?>
 	
-			<?= $form->field($usuario, 'txt_apellido_paterno',['options'=>['class'=>'animated']]) -> label(false) -> textInput(["placeholder" => "Apellido paterno", "class"=>"input-short animateds"])?>
+			<?= $form->field($usuario, 'txt_apellido_paterno',['options'=>['class'=>'animated']]) -> textInput(["placeholder" => "Apellido paterno", "class"=>"input-short animateds"])?>
 			
-			<?= $form->field($usuario, 'fch_nacimiento',['options'=>['class'=>'animated']]) -> label(false) -> textInput(["placeholder" => "Fecha de nacimiento", "class"=>"input-short animateds"])?>
+			<?= $form->field($usuario, 'fch_nacimiento',['options'=>['class'=>'animated']])-> textInput(["placeholder" => "Fecha de nacimiento", "class"=>"input-short animateds"])?>
 	
-			<?= $form->field($usuario, 'tel_numero_celular',['options'=>['class'=>'animated']]) -> label(false) -> textInput(["placeholder" => "Número celular", 'maxlength'=>10])?>
+			<?= $form->field($usuario, 'tel_numero_celular',['options'=>['class'=>'animated']])-> textInput(["placeholder" => "Número celular", 'maxlength'=>10])?>
 			
-			<?= $form->field($usuario, 'txt_cp',['options'=>['class'=>'animated']]) -> label(false) -> textInput(["placeholder" => "C. P.", 'maxlength'=>5])?>
+			<?= $form->field($usuario, 'txt_cp',['options'=>['class'=>'animated']]) -> textInput(["placeholder" => "C. P.", 'maxlength'=>5])?>
 		
-			<?= $form->field($usuario, 'txt_email',['options'=>['class'=>'animated']]) -> label(false) -> textInput(["placeholder" => "Email"])?>
+			<?= $form->field($usuario, 'txt_email',['options'=>['class'=>'animated']]) -> textInput(["placeholder" => "Email"])?>
 			
 			<?= $form->field($premio, 'txt_token') -> label(false) ->hiddenInput()?>
 			
